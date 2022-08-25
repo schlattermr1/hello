3a2b36a8980d2747cf54216d52aa29e3d27a17e9
Matt Schlatter <mschlatter2@huskers.unl.edu>
2022-08-25 17:38:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Matt Schlatter
+ *
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Matt Schlatter, Computer Engineering\n");
+
+  return 0;
+}
